848fc244a9cd5562a76f93f24b8344fd0691cfca
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2022-01-05 09:50:10 +0200

    more error cleaning

--- Algo.c
+++ Algo.c
@@ -2,56 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void init_path_mat(pathMat* M, Graph* g){
-    
-    unsigned int inf  = -1; //max value that can be held by an int
+void init_path_mat(pathMat *M, Graph *g) {
+    unsigned int inf = -1; //max value that can be held by an int
 
-    M -> dimantions = g -> V;
-
-    M -> weightMat = (int*) malloc(sizeof(int) * g -> V * g -> V);
-    M -> preMat = (int*) malloc(sizeof(int) * g -> V * g -> V);
+    M->dimantions = g->V;
+    M->weightMat = (int *) malloc(sizeof(int) * g->V * g->V);
+    M->preMat = (int *) malloc(sizeof(int) * g->V * g->V);
 
     //initialising every value to inf
-    for(int i = 0; i < g -> V; i++){
-        for(int j = 0; j < g -> V; j++){
+    for (int i = 0; i < g->V; i++) {
+        for (int j = 0; j < g->V; j++) {
 
-            M -> weightMat[(g -> V * i) + j] = inf;
-            M -> preMat[(g -> V * i) + j] = -1;
+            M->weightMat[(g->V * i) + j] = inf;
+            M->preMat[(g->V * i) + j] = -1;
         }
     }
 
     //executing first step of Fluid Warshall algorithm
-    Node *n =  g -> start;
+    Node *n = g->start;
 
-    while(n != NULL){
-
+    while (n != NULL) {
         Edge *e = n->firstEdge;
+        while (e != NULL) {
 
-        while(e != NULL){
+            M->weightMat[(g->V * n->id) + e->destNode->id] = e->weight;
+            M->preMat[(g->V * n->id) + e->destNode->id] = n->id;
 
-            M -> weightMat[(g -> V * n -> id) + e -> destNode -> id] = e -> weight;
-            M -> preMat[(g -> V * n -> id) + e -> destNode -> id] = n -> id;
-
-            e = e -> nextEdge;
+            e = e->nextEdge;
         }
-
-        n = n -> nextNode;
+        n = n->nextNode;
     }
 
 
     //executing Fluid Warshall
-    for(int i = 0; i < g -> V; i++){
-        for(int j = 0; j < g -> V; j++){
-            for(int k = 0; k < g -> V; k++){
+    for (int i = 0; i < g->V; i++) {
+        for (int j = 0; j < g->V; j++) {
+            for (int k = 0; k < g->V; k++) {
 
-                if(i != j){
+                if (i != j) {
 
-                    int visitK = M -> weightMat[(i * g->V) + k]  +  M -> weightMat[(k * g->V) + j];
+                    int visitK = M->weightMat[(i * g->V) + k] + M->weightMat[(k * g->V) + j];
 
-                    if( M -> weightMat[(i * g -> V) + j] > visitK){
+                    if (M->weightMat[(i * g->V) + j] > visitK) {
 
-                        M -> weightMat[(i * g -> V) + j] = visitK;
-                        M -> preMat[(i * g -> V) + j] = k;
+                        M->weightMat[(i * g->V) + j] = visitK;
+                        M->preMat[(i * g->V) + j] = k;
                     }
                 }
             }
@@ -60,105 +55,79 @@ void init_path_mat(pathMat* M, Graph* g){
 
 }
 
-void freeMats(pathMat* M){
+void freeMats(pathMat *M) {
+    free(M->preMat);
+    free(M->weightMat);
 
-    free(M ->preMat);
-    free(M -> weightMat);
-
-    M -> preMat = NULL;
-    M -> weightMat = NULL;
+    M->preMat = NULL;
+    M->weightMat = NULL;
 }
 
-int distance(int src, int dest, pathMat* M){ return M -> weightMat[(M -> dimantions * src) + dest];}
+int distance(int src, int dest, pathMat *M) { return M->weightMat[(M->dimantions * src) + dest]; }
 
-void addToPath(path* P, int id){
+void addToPath(path *P, int id) {
+    Node *n = (Node *) malloc(sizeof(Node));
+    n->id = id;
+    n->nextNode = NULL;
+    n->firstEdge = NULL;
 
-    Node *n = (Node*) malloc(sizeof(Node));
+    Node *c = P->head;
 
-    n -> id = id; n -> nextNode = NULL; n -> firstEdge = NULL;
-
-    Node* c = P -> head;
-
-    if( c == NULL){
-
-        P -> head = n;
-        
-    }else{
-
-        n -> nextNode = c;
-        P -> head = n;
-
-    }    
-}
-
-void mergePaths(path* p1, path* p2){
-
-    Node *n = p1 -> head;
-
-    if(n == NULL){ p1 -> head = p2 -> head; return;}
-
-    while(n -> nextNode != NULL){
-        n = n -> nextNode;
+    if (c == NULL) {
+        P->head = n;
+    } else {
+        n->nextNode = c;
+        P->head = n;
     }
-
-    n -> nextNode = p2 -> head;
 }
 
-path* shortestPath(int src, int dest, pathMat* M){
+void mergePaths(path *p1, path *p2) {
+    Node *n = p1->head;
+    if (n == NULL) {
+        p1->head = p2->head;
+        return;
+    }
+    while (n->nextNode != NULL) {
+        n = n->nextNode;
+    }
+    n->nextNode = p2->head;
+}
 
-    path* P = (path*) malloc(sizeof(path));
+path *shortestPath(int src, int dest, pathMat *M) {
 
+    path *P = (path *) malloc(sizeof(path));
     addToPath(P, dest);
 
     int curr = dest;
-
-    while( curr != src){
-
-        int last = M -> preMat[( M -> dimantions * src) + curr];
-
-        if( last == -1){ return NULL;}        }
-
-        addToPath(last);
-
-        curr = last; 
+    while (curr != src) {
+        int last = M->preMat[(M->dimantions * src) + curr];
+        if (last == -1) { return NULL; }
+        addToPath(P, last);
+        curr = last;
     }
-
     return P;
 }
 
-void removeDoubles(path* P){
-
-    Node* n = P -> head;
-
-    while( n -> nextNode != NULL){
-
-        if( n -> id == n -> nextNode -> id){
-
-            n -> nextNode = n -> nextNode -> nextNode;
-
-        }else{
-
-            n = n -> nextNode;
+void removeDoubles(path *P) {
+    Node *n = P->head;
+    while (n->nextNode != NULL) {
+        if (n->id == n->nextNode->id) {
+            n->nextNode = n->nextNode->nextNode;
+        } else {
+            n = n->nextNode;
         }
     }
 }
 
-path* TSP(path* cities){
+path *TSP(path *cities) {
+    Node *n = cities->head;
+    path *P = (path *) malloc(sizeof(path));
 
-    Node* n = cities -> head;
-
-    path* P = (path*) malloc(sizeof(path));
-
-    while(n -> nextNode != NULL){
-
-        path* P2 = (path*) malloc(sizeof(path));
-
-        P2 = shortestPath(P2, n -> id, n -> nextNode -> id);
-
+    while (n->nextNode != NULL) {
+        path *P2 = (path *) malloc(sizeof(path));
+        P2 = shortestPath(n->id, n->nextNode->id,P2 );// what the parameters supposed to be?
         mergePaths(P, P2);
     }
-
     removeDoubles(P);
-
     return P;
 }
